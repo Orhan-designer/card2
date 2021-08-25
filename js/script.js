566584336b86a572cfb32248cef2a9b9a67660ca
Orhan <ya.eskimo-callboy@yandex.by>
2021-08-26 02:16:51 +0300

    delete not used code in HTML and add new code, delete old CSS styles and add new, delete old scripts and add new

--- js/script.js
+++ js/script.js
@@ -1,129 +1,131 @@
 //Расчёт рейтинга (второй способ)
 //Создаём код со свойствами для всей карточки
-let card = {
-  //создаём объект для всей карточки
-  poster: "poster.jpg",
-  type: "type.png",
-  sale: "sale.png",
-  title: "Тайная жизнь,",
-  subtitle: "домашних животных 2",
-  price: 50,
-  currency: "руб.",
-  rating: 4,
-  buy: "order.jpg",
-  act: true
-};
+let card = [
+  {
+    //создаём объект для всех карточки
+    poster: "poster.jpg",
+    type: "type.png",
+    sale: "sale.png",
+    title: "Тайная жизнь,",
+    subtitle: "домашних животных 2",
+    price: 50,
+    currency: "руб.",
+    rating: 3,
+    buy: "order.jpg",
+    act: true,
+  },
 
-//Создаём код со свойствами для всей toy
-let toy = {
-  //создаём объект для всей карточки
-  poster: "toy4.jpg",
-  title: "История игрушек 4",
-  subtitle: "Приключения Вилкинса",
-  price: 60,
-  currency: "руб.",
-  rating: 4,
-  buy: "order.jpg",
-  act: true
-};
+  {
+    poster: "toy4.jpg",
+    type: "type.png",
+    sale: "sale.png",
+    title: "История игрушек 4",
+    subtitle: "Приключения Вилкинса",
+    price: 60,
+    currency: "руб.",
+    rating: 3,
+    buy: "order.jpg",
+    act: false,
+  },
 
-//Создаём код со свойствами для всей ferd
-let ferdinard = {
-  //создаём объект для всей карточки
-  poster: "ferd.jpg",
-  title: "Фердинанд",
-  subtitle: "Побег из коровника",
-  price: 40,
-  currency: "руб.",
-  rating: 4,
-  buy: "order.jpg",
-  act: true
-};
+  {
+    poster: "ferd.jpg",
+    type: 'type.png',
+    sale: 'sale.png',
+    title: "Фердинанд",
+    subtitle: "Побег из коровника",
+    price: 40,
+    currency: "руб.",
+    rating: 3,
+    buy: "order.jpg",
+    act: true,
+  },
+];
 
-//Полная стоимость
-let fullPrice = `${card.price},00 ${card.currency}`;
-let toyFullPrice = `${toy.price},00 ${toy.currency}`;
-let ferFullPrice = `${ferdinard.price},00 ${ferdinard.currency}`;
+let s = "";
 
-//Расчёт акции
-let salePrice = `${Math.round(card.price * 0.9)},00${card.currency}`; //округляем до целого числа
-let saleToyPrice = `${Math.round(toy.price * 0.9)},00${toy.currency}`;
-let saleFerPrice = `${Math.round(ferdinard.price * 0.9)},00${ferdinard.currency}`;
+for (let i = 0; i < card.length; i++) {
+  //Полная стоимость
+  let fullPrice = `${card[i].price},00 ${card[i].currency}`;
 
-let stars = "";
-switch (
-  card.rating //сюда вставляем значения из объекста card и проверяем его
-) {
-  case 1:
-    stars =
-      '<span class="material-icons">star star_border star_border star_border star_border</span>';
-    break;
+  //Расчёт акции
+  let salePrice = `${Math.round(card[i].price * 0.9)},00${card[i].currency}`; //округляем до целого числа
 
-  case 1.5:
-    stars =
-      '<span class="material-icons">star star_half star_border star_border star_border</span>';
-    break;
+  //Расчёт рейтинга
+  let stars = "";
+  switch (
+    card[i].rating //сюда вставляем значения из объекста card и проверяем его
+  ) {
+    case 1:
+      stars =
+        'star star_border star_border star_border star_border';
+      break;
 
-  case 2:
-    stars =
-      '<span class="material-icons">star star star_border star_border star_border</span>';
-    break;
+    case 1.5:
+      stars =
+        'star star_half star_border star_border star_border';
+      break;
 
-  case 2.5:
-    stars =
-      '<span class="material-icons">star star star_half star_border star_border</span>';
-    break;
+    case 2:
+      stars =
+        'star star star_border star_border star_border';
+      break;
 
-  case 3:
-    stars =
-      '<span class="material-icons">star star star star_border star_border</span>';
-    break;
+    case 2.5:
+      stars =
+        'star star star_half star_border star_border';
+      break;
 
-  case 3.5:
-    stars =
-      '<span class="material-icons">star star star star_half star_border</span>';
-    break;
+    case 3:
+      stars =
+        'star star star star_border star_border';
+      break;
 
-  case 4:
-    stars = '<span class="material-icons">star star star star star_border</span>';
-    break;
+    case 3.5:
+      stars =
+        'star star star star_half star_border';
+      break;
 
-  case 4.5:
-    stars = '<span class="material-icons">star star star star star_half</span>';
-    break;
+    case 4:
+      stars =
+        'star star star star star_border';
+      break;
 
-  case 5:
-    stars = '<span class="material-icons">star star star star star</span>';
-    break;
+    case 4.5:
+      stars =
+        'star star star star star_half';
+      break;
 
-  default:
-    stars =
-      '<span class="material-icons">star star_border star_border star_border star_border</span>';
-}
+    case 5:
+      stars = 'star star star star star';
+      break;
 
-let finalPrice = "";
-if (card.act) {
-  finalPrice = `<div>${salePrice}</div> <div>${fullPrice}</div>`;
-} else finalPrice = `<div>${fullPrice}</div>`;
+    default:
+      stars =
+        'star_border star_border star_border star_border star_border';
+  }
 
-let toyPrice = "";
-if (toy.act) {
-  toyPrice = `<div>${saleToyPrice}</div> <div>${toyFullPrice}</div>`;
-} else toyPrice = `<div>${toyFullPrice}</div>`;
+  let finalPrice = "", saleImg = "";
 
-let ferPrice = "";
-if (ferdinard.act) {
-  ferPrice = `<div>${saleFerPrice}</div> <div>${ferFullPrice}</div>`;
-} else ferPrice = `<div>${ferFullPrice}</div>`;
+  if (card[i].act) {
+    finalPrice = `<div>${salePrice}</div> 
+                  <div>${fullPrice}</div>`;
+    saleImg = `<img style="opacity: 1" src="./images/${card[i].sale}" alt="">`;
+  } else  {
+    finalPrice = `<div>${fullPrice}</div>`;
+    saleImg = `<img style="opacity: 0" src="./images/${card[i].sale}" alt=""`;
+  }
 
-let s = `
+  s += `
   <div id="card">
     <div class="poster">
-      <img src="./images/${card.poster}" alt="">
+      <img src="./images/${card[i].poster}" alt="">
+      ${saleImg}
+      <img src="./images/${card[i].type}">
     </div>
     <div class="title">
-      <div>${card.title}</div>
-      <div>${card.subtitle}</div>
+      <div>${card[i].title}</div>
+      <div>${card[i].subtitle}</div>
     </div>
     <div class="price">
       ${finalPrice}
@@ -131,55 +133,14 @@ let s = `
     <div class="rating">
       <span class="material-icons">${stars}</span>
     </div>
-    <button>
-      <a href="#"><img src="./images/${card.buy}" class="hue"></a>
-    </button>
-    <div class="prelative">
-      <img src="./images/${card.type}" alt="">
-      <img src="./images/${card.sale}" alt="" id="sale">
+    <div class="order">
+      <a href="#"><img src="./images/${card[i].buy}" class="hue"></a>
     </div>
   </div>
-  <div id="toy">
-    <div class="poster">
-      <img src="./images/${toy.poster}" alt="">
-    </div>
-    <div class="title">
-      <div>${toy.title}</div>
-      <div>${toy.subtitle}</div>
-    </div>
-    <div class="price">
-      ${toyPrice}
-    </div>
-    <div class="rating">
-      <span class="material-icons">${stars}</span>
-    </div>
-    <button>
-      <a href="#"><img src="./images/${card.buy}" class="hue"></a>
-    </button>
-  </div>
-  <div id="ferdinard">
-    <div class="poster">
-      <img src="./images/${ferdinard.poster}" alt="">
-    </div>
-    <div class="title">
-      <div>${ferdinard.title}</div>
-      <div>${ferdinard.subtitle}</div>
-    </div>
-    <div class="price">
-      ${ferPrice}
-    </div>
-    <div class="rating">
-      <span class="material-icons">${stars}</span>
-    </div>
-    <button>
-      <a href="#"><img src="./images/${ferdinard.buy}" class="hue"></a>
-    </button>
-  </div> 
 `;
+}
 
 cards.innerHTML = s;
 
-if (card.act) sale.style.opacity = "1";
-else sale.style.opacity = "0";
-
-
+/* if (card.act) sale.style.opacity = "1";
+else sale.style.opacity = "0"; */
